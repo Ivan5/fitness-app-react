13bfc98aedd3ba4f62736eb3567ba3bf4af1929a
Ivan5 <converse_ivan@hotmail.com>
2019-06-20 15:56:23 -0500

    Add props to component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from "react";
 import ReactDom from "react-dom";
 import Card from "./components/Card";
 import "bootstrap/dist/css/bootstrap.css";
+import exerciseImg from "./images/exercise.png";
 const container = document.getElementById("root");
 
-ReactDom.render(<Card />, container);
+ReactDom.render(
+  <Card
+    title="Technique Guides"
+    description="Learn amazing street workout and calisthenics"
+    img={exerciseImg}
+    leftColor="#a74cf2"
+    rightColor="#617bfb"
+  />,
+  container
+);
