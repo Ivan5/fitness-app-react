3898741660d67b6e849ac7d1c259800cd77eeaa5
Ivan5 <converse_ivan@hotmail.com>
2019-06-27 14:27:03 -0500

    Make a personal hook

--- src/pages/Exercises.js
+++ src/pages/Exercises.js
@@ -1,30 +1,14 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { Component } from "react";
 import ExerciseList from "../components/ExerciseList";
 import Welcome from "../components/Welcome";
 import AddButton from "../components/AddButton";
 import Loading from "../components/Loading";
+import useFetch from "../hooks/useFetch";
 
 const Exercises = () => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchExercises = async () => {
-      try {
-        let res = await fetch("http://localhost:8000/api/exercises");
-        let data = await res.json();
-        setData(data);
-        setLoading(false);
-      } catch (error) {
-        setLoading(false);
-        setError(error);
-      } finally {
-      }
-    };
-    fetchExercises();
-  }, []);
-
+  const { data, loading, error } = useFetch(
+    "http://localhost:8000/api/exercises"
+  );
   if (loading) return <Loading />;
   return (
     <>
